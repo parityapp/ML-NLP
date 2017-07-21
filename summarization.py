4b5ad1e15737c076f498a8ef516505d8a170cf9f
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-07-22 00:20:05 +0200

    Load example function

--- summarization.py
+++ summarization.py
@@ -12,13 +12,26 @@ This module contains the routines for:
 
 from typing import List, Dict, NewType
 from datetime import datetime
+import json
+
 
 Message = Dict[str, datetime]
 
 
+def load_sample(file='sample.json'):
+    with open(file, 'r') as f:
+        return json.load(f)
+
+
 def representative_msgs(messages: List[Message]) -> List[Message]:
+    print(messages)
     pass
 
 
 def summarize_msgs(messages: List[Message]) -> str:
+    print(messages)
     pass
+
+
+if __name__ == '__main__':
+    representative_msgs(load_sample())
