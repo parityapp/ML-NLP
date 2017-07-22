637fcaaf338175b5915d8920b765e5508909e019
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-07-22 17:56:30 +0200

    Fix topic_extraction for PEP8 compliance

--- topic_extraction.py
+++ topic_extraction.py
@@ -10,84 +10,89 @@ import nltk
 import string
 from summarization import load_sample_text
 
+
 def isPunct(word):
-  return len(word) == 1 and word in string.punctuation
+    return len(word) == 1 and word in string.punctuation
+
 
 def isNumeric(word):
-  try:
-    float(word) if '.' in word else int(word)
-    return True
-  except ValueError:
-    return False
+    try:
+        float(word) if '.' in word else int(word)
+        return True
+    except ValueError:
+        return False
+
 
 class RakeKeywordExtractor:
 
-  def __init__(self):
-    self.stopwords = set(nltk.corpus.stopwords.words())
-    self.top_fraction = 1 # consider top third candidate keywords by score
+    def __init__(self):
+        self.stopwords = set(nltk.corpus.stopwords.words())
+        self.top_fraction = 1  # consider top third candidate keywords by score
 
-  def _generate_candidate_keywords(self, sentences):
-    phrase_list = []
-    for sentence in sentences:
-      words = map(lambda x: "|" if x in self.stopwords else x,
-        nltk.word_tokenize(sentence.lower()))
-      phrase = []
-      for word in words:
-        if word == "|" or isPunct(word):
-          if len(phrase) > 0:
-            phrase_list.append(phrase)
+    def _generate_candidate_keywords(self, sentences):
+        phrase_list = []
+        for sentence in sentences:
+            words = map(lambda x: "|" if x in self.stopwords else x,
+                        nltk.word_tokenize(sentence.lower()))
             phrase = []
+            for word in words:
+                if word == "|" or isPunct(word):
+                    if len(phrase) > 0:
+                        phrase_list.append(phrase)
+                        phrase = []
+                else:
+                    phrase.append(word)
+        return phrase_list
+
+    def _calculate_word_scores(self, phrase_list):
+        word_freq = nltk.FreqDist()
+        word_degree = nltk.FreqDist()
+        for phrase in phrase_list:
+            degree = len(list(filter(lambda x: not isNumeric(x), phrase))) - 1
+            for word in phrase:
+                # other words
+                word_freq[word] += 1
+                word_degree[word, degree] += 1
+
+        for word in word_freq.keys():
+            word_degree[word] = word_degree[word] + word_freq[word]  # itself
+            # word score = deg(w) / freq(w)
+        word_scores = {}
+        for word in word_freq.keys():
+            word_scores[word] = word_degree[word] / word_freq[word]
+        return word_scores
+
+    def _calculate_phrase_scores(self, phrase_list, word_scores):
+        phrase_scores = {}
+        for phrase in phrase_list:
+            phrase_score = 0
+            for word in phrase:
+                phrase_score += word_scores[word]
+            phrase_scores[" ".join(phrase)] = phrase_score
+        return phrase_scores
+
+    def extract(self, text, incl_scores=False):
+        sentences = nltk.sent_tokenize(text)
+        phrase_list = self._generate_candidate_keywords(sentences)
+        word_scores = self._calculate_word_scores(phrase_list)
+        phrase_scores = self._calculate_phrase_scores(
+            phrase_list, word_scores)
+        sorted_phrase_scores = sorted(phrase_scores.items(),
+                                      key=operator.itemgetter(1), reverse=True)
+        n_phrases = len(sorted_phrase_scores)
+        if incl_scores:
+            return sorted_phrase_scores[0:int(n_phrases/self.top_fraction)]
         else:
-          phrase.append(word)
-    return phrase_list
+            return map(lambda x: x[0],
+                       sorted_phrase_scores[0:int(n_phrases/self.top_fraction)])
 
-  def _calculate_word_scores(self, phrase_list):
-    word_freq = nltk.FreqDist()
-    word_degree = nltk.FreqDist()
-    for phrase in phrase_list:
-      degree = len(list(filter(lambda x: not isNumeric(x), phrase))) - 1
-      for word in phrase:
-      # other words
-        word_freq[word]+=1
-        word_degree[word,degree]+=1
-    for word in word_freq.keys():
-      word_degree[word] = word_degree[word] + word_freq[word] # itself
-    # word score = deg(w) / freq(w)
-    word_scores = {}
-    for word in word_freq.keys():
-      word_scores[word] = word_degree[word] / word_freq[word]
-    return word_scores
-
-  def _calculate_phrase_scores(self, phrase_list, word_scores):
-    phrase_scores = {}
-    for phrase in phrase_list:
-      phrase_score = 0
-      for word in phrase:
-        phrase_score += word_scores[word]
-      phrase_scores[" ".join(phrase)] = phrase_score
-    return phrase_scores
-    
-  def extract(self, text, incl_scores=False):
-    sentences = nltk.sent_tokenize(text)
-    phrase_list = self._generate_candidate_keywords(sentences)
-    word_scores = self._calculate_word_scores(phrase_list)
-    phrase_scores = self._calculate_phrase_scores(
-      phrase_list, word_scores)
-    sorted_phrase_scores = sorted(phrase_scores.items(),
-      key=operator.itemgetter(1), reverse=True)
-    n_phrases = len(sorted_phrase_scores)
-    if incl_scores:
-      return sorted_phrase_scores[0:int(n_phrases/self.top_fraction)]
-    else:
-      return map(lambda x: x[0],
-        sorted_phrase_scores[0:int(n_phrases/self.top_fraction)])
 
 def test():
-  rake = RakeKeywordExtractor()
-  text=load_sample_text(lines=40)
-  keywords = rake.extract(text, incl_scores=True)
-  return [t[0] for t in keywords[:5]]
-	
-  
+    rake = RakeKeywordExtractor()
+    text = load_sample_text(lines=40)
+    keywords = rake.extract(text, incl_scores=True)
+    return [t[0] for t in keywords[:5]]
+
+
 if __name__ == "__main__":
-  print(test())
+    print(test())
