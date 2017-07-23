26528d44bba3dd2cbe310345051c50c6bbe5241c
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-07-23 02:43:41 +0200

    Refactor ML backend to support multiple implementations

--- nlp.py
+++ nlp.py
@@ -10,13 +10,27 @@ This module contains the routines for:
 
 """
 
-from typing import List, Dict, NewType
 from datetime import datetime
 import dateutil.parser
 import json
+from typing import List, Dict
 
+from ntlk_rake import topic_extraction_rake
+
+from gensim.summarization import summarize
+from gensim.summarization import keywords
+
+from textrank import representative_msgs_textrank
+
+from lex_and_kl_summary import summarize_kl, summarize_lexpagerank
 
 Message = Dict[str, datetime]
+
+
+def whole_text(messages, lines=None):
+    if lines is not None:
+        messages = messages[:lines]
+    return ". ".join(d['content'] for d in messages)
 
 
 def load_sample(file='data/sample_chat.json'):
@@ -30,19 +44,31 @@ def load_sample(file='data/sample_chat.json'):
 
 
 def load_sample_text(file='data/sample_chat.json', lines=None):
-    messages = [d['content'] for d in load_sample(file)]
-    if lines is not None:
-        messages = messages[:lines]
-    return ". ".join(messages)
+    return whole_text(load_sample(text), lines=lines)
 
 
-def representative_msgs(messages: List[Message]) -> List[Message]:
-    pass
+def representative_msgs(messages: List[Message], method) -> List[Message]:
+
+    if method == 'textrank':
+        return representative_msgs_texrank(messages)
 
 
-def summarize_msgs(messages: List[Message]) -> str:
-    pass
+def summarize_msgs(messages: List[Message], method) -> str:
+    text = whole_text(messages)
+
+    if method == 'gensim':
+        return summarize(text)
+    elif method == 'lexrank':
+        summarize_lexpagerank(text)
+    elif metod == 'kl':
+        summarize_kl(text)
 
 
-def relevant_topics(messages: List[Message]) -> List[str]:
-    pass
+def relevant_topics(messages: List[Message], method) -> List[str]:
+
+    text = whole_text(messages)
+
+    if method == 'RAKE':
+        return topic_extraction_rake(text)
+    elif method == 'gensim':
+        return keywords(text)
