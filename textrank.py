3ae05844fa3e0bc5db329c8c16c99132e33377b9
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-07-23 06:37:33 +0200

    Fix textrank so it works with message format

--- textrank.py
+++ textrank.py
@@ -16,9 +16,12 @@ import numpy as np
 from nltk import sent_tokenize
 
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+from utils import load_sample
 
 
-def textrank(document, tokenize=True):
+def textrank(document, tokenize=True, num_msgs=5):
     if tokenize:
         sentences = sent_tokenize(document)
     else:
@@ -27,13 +30,18 @@ def textrank(document, tokenize=True):
     bow_matrix = CountVectorizer().fit_transform(sentences)
     normalized = TfidfTransformer().fit_transform(bow_matrix)
 
-    similarity_graph = normalized * normalized.T
+    # similarity_graph = normalized * normalized.T
+    # nx_graph = nx.from_scipy_sparse_matrix(similarity_graph)
 
-    nx_graph = nx.from_scipy_sparse_matrix(similarity_graph)
+    similarity_graph = cosine_similarity(normalized)
+    nx_graph = nx.from_numpy_matrix(similarity_graph)
+
     scores = nx.pagerank(nx_graph)
-    sentence_array = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+    sentence_array = sorted(((scores[i], s, i) for i, s in enumerate(sentences)), reverse=True)
 
     sentence_array = np.asarray(sentence_array)
+
+    # print(sentence_array[:10])
 
     fmax = float(sentence_array[0][0])
     fmin = float(sentence_array[len(sentence_array) - 1][0])
@@ -48,19 +56,36 @@ def textrank(document, tokenize=True):
 
     threshold = (sum(temp_array) / len(temp_array)) + 0.2
 
+    # print(temp_array[:10])
+
     sentence_list = []
 
     for i in range(0, len(temp_array)):
         if temp_array[i] > threshold:
             sentence_list.append(sentence_array[i][1])
 
+    # print(sentence_list[:10])
+
+    sentence_list = sentence_list[:num_msgs]
+
     seq_list = []
-    for sentence in sentences:
+    positions = []
+    for sentence, position in zip(sentences, range(len(sentences))):
         if sentence in sentence_list:
             seq_list.append(sentence)
+            positions.append(position)
 
-    return seq_list
+    return seq_list, positions
 
 
-def representative_msgs_textrank(messages):
-    pass
+def representative_msgs_textrank(messages, num_msgs=5):
+    contents = [d['content'] for d in messages]
+    sentences, positions = textrank(contents, tokenize=False, num_msgs=num_msgs)
+    return [messages[i] for i in positions]
+
+
+if __name__ == '__main__':
+    messages = load_sample()
+    for m in representative_msgs_textrank(load_sample()):
+        print(m)
+        pass
