f6f92334f47874ebf68f6a3d2e7fc2f5a1e14296
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-07-23 03:41:47 +0200

    Fix a lot of style violations for mglda

--- mglda.py
+++ mglda.py
@@ -1,49 +1,49 @@
 #!/usr/local/bin/python
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 
 # mg-lda
-# This code is available under the MIT License.
-# (c)2012 Masanao Ochi.
+ # This code is available under the MIT License.
+ # (c)2012 Masanao Ochi.
 
 import numpy
 
 class MGLDA:
     def __init__(self, K_gl, K_loc, gamma, alpha_gl, alpha_loc, alpha_mix_gl, alpha_mix_loc, beta_gl, beta_loc, T, docs, W, smartinit=False):
-        
+
         self.K_gl = K_gl
         self.K_loc = K_loc
-        
+
         self.gamma         = gamma
-        self.alpha_gl      = alpha_gl # parameter of topics prior
+        self.alpha_gl      = alpha_gl  # parameter of topics prior
         self.alpha_loc     = alpha_loc
         self.alpha_mix_gl  = alpha_mix_gl
         self.alpha_mix_loc = alpha_mix_loc
-        
-        self.beta_gl = beta_gl # parameter of words prior
+
+        self.beta_gl = beta_gl  # parameter of words prior
         self.beta_loc = beta_loc
-        
-        self.T = T # sliding window width
-        
+
+        self.T = T  # sliding window width
+
         self.docs = docs
         self.W = W
 
-        self.v_d_s_n     = [] #sumi
-        self.r_d_s_n     = [] #sumi
-        self.z_d_s_n     = [] #sumi
-        
+        self.v_d_s_n     = []  # sumi
+        self.r_d_s_n     = []  # sumi
+        self.z_d_s_n     = []  # sumi
+
         self.n_gl_z_w    = numpy.zeros((self.K_gl, self.W))
         self.n_gl_z      = numpy.zeros(self.K_gl)
-        self.n_d_s_v     = [] #sumi
-        self.n_d_s       = [] #sumi
-        self.n_d_v_gl    = [] #sumi
-        self.n_d_v       = [] #sumi
+        self.n_d_s_v     = []  # sumi
+        self.n_d_s       = []  # sumi
+        self.n_d_v_gl    = []  # sumi
+        self.n_d_v       = []  # sumi
         self.n_d_gl_z    = numpy.zeros((len(self.docs), self.K_gl))
         self.n_d_gl      = numpy.zeros((len(self.docs)))
 
         self.n_loc_z_w   = numpy.zeros((self.K_loc, self.W))
         self.n_loc_z     = numpy.zeros(self.K_loc)
-        self.n_d_v_loc   = [] #sumi
-        self.n_d_v_loc_z = [] #sumi
+        self.n_d_v_loc   = []  # sumi
+        self.n_d_v_loc_z = []  # sumi
 
         self.inflation = 0
 
@@ -52,75 +52,75 @@ class MGLDA:
             v_d = []
             r_d = []
             z_d = []
-            
+
             n_d_s_v_d = []
             n_d_s_d = []
-            
+
             n_d_v_gl_v = []
             n_d_v_v = []
             n_d_v_loc_v = []
             n_d_v_loc_z_v = []
             for v in range(self.T+len(doc)-1):
-                n_d_v_gl_v.append(self.inflation) # initialize word count with global topic for each sliding window
-                n_d_v_v.append(self.inflation) # initialize word count for each sliding window
-                n_d_v_loc_v.append(self.inflation) # initialize word count with local topic for each sliding window
-                
+                n_d_v_gl_v.append(self.inflation)  # initialize word count with global topic for each sliding window
+                n_d_v_v.append(self.inflation)  # initialize word count for each sliding window
+                n_d_v_loc_v.append(self.inflation)  # initialize word count with local topic for each sliding window
+
                 n_d_v_loc_z_z = []
                 for k in range(self.K_loc):
-                    n_d_v_loc_z_z.append(self.inflation) # initialize word count assigned local topic for each sliding window
-                n_d_v_loc_z_v.append(n_d_v_loc_z_z) 
-            
+                    n_d_v_loc_z_z.append(self.inflation)  # initialize word count assigned local topic for each sliding window
+                n_d_v_loc_z_v.append(n_d_v_loc_z_z)
+
             self.n_d_v_gl.append(n_d_v_gl_v)
             self.n_d_v.append(n_d_v_v)
             self.n_d_v_loc.append(n_d_v_loc_v)
-            
+
             self.n_d_v_loc_z.append(n_d_v_loc_z_v)
-            
+
             for s, sent in enumerate(doc):
                 v_s = []
                 r_s = []
                 z_s = []
                 for i, word in enumerate(sent):
-                    v     = numpy.random.randint(0, self.T) # initialize sliding window for each word
+                    v = numpy.random.randint(0, self.T)  # initialize sliding window for each word
                     v_s.append(v)
-                    
-                    r_int = numpy.random.randint(0, 2) # initialize topic category
+
+                    r_int = numpy.random.randint(0, 2)  # initialize topic category
                     r = ""
                     if r_int == 0:
                         r = "gl"
                     else:
                         r = "loc"
-                    r_s.append(r) 
-                    
+                    r_s.append(r)
+
                     z = 0
                     if r == "gl":
-                        z = numpy.random.randint(0, self.K_gl) # initialize global topic
+                        z = numpy.random.randint(0, self.K_gl)  # initialize global topic
                     else:
-                        z = numpy.random.randint(0, self.K_loc) # initialize local topic
+                        z = numpy.random.randint(0, self.K_loc)  # initialize local topic
                     z_s.append(z)
                 v_d.append(v_s)
                 r_d.append(r_s)
                 z_d.append(z_s)
-                
+
                 n_d_s_v_s = []
                 for v in range(self.T):
-                    n_d_s_v_s.append(self.inflation) # initialize n_d_s_v
+                    n_d_s_v_s.append(self.inflation)  # initialize n_d_s_v
                 n_d_s_v_d.append(n_d_s_v_s)
-                
-                n_d_s_d.append(self.inflation) # initialize n_d_s
-            
+
+                n_d_s_d.append(self.inflation)  # initialize n_d_s
+
             self.v_d_s_n.append(v_d)
             self.r_d_s_n.append(r_d)
             self.z_d_s_n.append(z_d)
-            
+
             self.n_d_s_v.append(n_d_s_v_d)
             self.n_d_s.append(n_d_s_d)
-        
+
         print ("initialize")
         for m, doc in enumerate(self.docs):
             for s, sent in enumerate(doc):
                 for i, word in enumerate(sent):
-                    v = self.v_d_s_n[m][s][i] # 0--T
+                    v = self.v_d_s_n[m][s][i]  # 0--T
                     r = self.r_d_s_n[m][s][i]
                     z = self.z_d_s_n[m][s][i]
                     if r == "gl":
@@ -148,11 +148,11 @@ class MGLDA:
         for m, doc in enumerate(self.docs):
             for s, sent in enumerate(doc):
                 for i, word in enumerate(sent):
-                    v = self.v_d_s_n[m][s][i] # 0--T
+                    v = self.v_d_s_n[m][s][i]  # 0--T
                     r = self.r_d_s_n[m][s][i]
                     z = self.z_d_s_n[m][s][i]
-                    
-                    # discount
+
+                     # discount
                     if r == "gl":
                         self.n_gl_z_w[z][word]      -= 1
                         self.n_gl_z[z]              -= 1
@@ -170,7 +170,7 @@ class MGLDA:
                     self.n_d_s_v[m][s][v]       -= 1
                     self.n_d_s[m][s]            -= 1
                     self.n_d_v[m][s+v]          -= 1
-                    
+
                     # sampling topic new_z for t
                     p_v_r_z = []
                     label_v_r_z = []
@@ -186,7 +186,7 @@ class MGLDA:
                             term4 = float(self.n_d_gl_z[m][z_t] + self.alpha_gl) / (self.n_d_gl[m] + self.K_gl*self.alpha_gl)
                             score = term1 * term2 * term3 * term4
                             p_v_r_z.append(score)
-                        # for r == "loc" 
+                        # for r == "loc"
                         for z_t in range(self.K_loc):
                             label = [v_t, "loc", z_t]
                             label_v_r_z.append(label)
@@ -197,12 +197,12 @@ class MGLDA:
                             term4 = float(self.n_d_v_loc_z[m][s+v_t][z_t] + self.alpha_loc) / (self.n_d_v_loc[m][s+v_t] + self.K_loc*self.alpha_loc)
                             score = term1 * term2 * term3 * term4
                             p_v_r_z.append(score)
-                    
+
                     np_p_v_r_z = numpy.array(p_v_r_z)
                     new_p_v_r_z_idx = numpy.random.multinomial(1, np_p_v_r_z / np_p_v_r_z.sum()).argmax()
                     new_v, new_r, new_z = label_v_r_z[new_p_v_r_z_idx]
-                    
-                    # update
+
+                     # update
                     if new_r == "gl":
                         self.n_gl_z_w[new_z][word]          += 1
                         self.n_gl_z[new_z]                  += 1
@@ -220,7 +220,7 @@ class MGLDA:
                     self.n_d_s_v[m][s][new_v]               += 1
                     self.n_d_s[m][s]                        += 1
                     self.n_d_v[m][s+new_v]                  += 1
-                    
+
                     self.v_d_s_n[m][s][i] = new_v
                     self.r_d_s_n[m][s][i] = new_r
                     self.z_d_s_n[m][s][i] = new_z
@@ -229,6 +229,7 @@ class MGLDA:
         """get topic-word distribution"""
         return (self.n_gl_z_w + 1) / (self.n_gl_z[:, numpy.newaxis] + 1), (self.n_loc_z_w + 1) / (self.n_loc_z[:, numpy.newaxis] + 1)
 
+
 def mglda_learning(mglda, iteration, voca):
     for i in range(iteration):
         print(("\n\n\n==== " + str(i) + "-th inference ===="))
@@ -236,16 +237,17 @@ def mglda_learning(mglda, iteration, voca):
         print ("inference complete")
         output_word_topic_dist(mglda, voca)
 
+
 def output_word_topic_dist(mglda, voca):
     z_gl_count = numpy.zeros(mglda.K_gl, dtype=int)
     z_loc_count = numpy.zeros(mglda.K_loc, dtype=int)
     word_gl_count = [dict() for k in range(mglda.K_gl)]
     word_loc_count = [dict() for k in range(mglda.K_loc)]
-    
+
     for m, doc in enumerate(mglda.docs):
         for s, sent in enumerate(doc):
             for i, word in enumerate(sent):
-#                v = mglda.v_d_s_n[m][s][i] # 0--T
+                # v = mglda.v_d_s_n[m][s][i]  # 0--T
                 r = mglda.r_d_s_n[m][s][i]
                 z = mglda.z_d_s_n[m][s][i]
                 if r == "gl":
@@ -279,21 +281,22 @@ def output_word_topic_dist(mglda, voca):
             print(("%s: %f (%d)" % (voca[w], phi_loc[k,w], word_loc_count[k].get(w,0))))
 
 def test():
-#    import nltk.corpus
+ #    import nltk.corpus
     import vocabulary_for_mglda as vocabulary
-    
-    #corpus = vocabulary.load_corpus_each_sentence("0:2000")
+
+     # corpus = vocabulary.load_corpus_each_sentence("0:2000")
     corpus = vocabulary.load_corpus_each_sentence("0:1")
-    #print (corpus)
-    #docs[sentence_idx][word_idx]
+     # print (corpus)
+     # docs[sentence_idx][word_idx]
     voca = vocabulary.Vocabulary(True)
     docs = [voca.doc_to_ids_each_sentence(doc) for doc in corpus]
     K_gl, K_loc, gamma, alpha_gl, alpha_loc, alpha_mix_gl, alpha_mix_loc, beta_gl, beta_loc, T, docs, W = 50, 10, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 3, docs, voca.size()
     mglda = MGLDA(K_gl, K_loc, gamma, alpha_gl, alpha_loc, alpha_mix_gl, alpha_mix_loc, beta_gl, beta_loc, T, docs, W)
     print(("corpus=%d, words=%d, K_gl=%d, K_loc=%d, gamma=%f, alpha_gl=%f, alpha_loc=%f, alpha_mix_gl=%f, alpha_mix_loc=%f, beta_gl=%f, beta_loc=%f" % (len(corpus), len(voca.vocas), K_gl, K_loc, gamma, alpha_gl, alpha_loc, alpha_mix_gl, alpha_mix_loc, beta_gl, beta_loc)))
-    
+
     iteration = 1000
     mglda_learning(mglda, iteration, voca)
 
 if __name__ == "__main__":
     test()
+    import vocabulary_for_mglda as vocabulary
--- vocabulary_for_mglda.py
+++ vocabulary_for_mglda.py
@@ -8,7 +8,9 @@ mglda用にwindowとかをつくります
 
 '''
 
-import nltk, re
+import nltk
+import re
+
 
 def load_corpus(range):
     m = re.match(r'(\d+):(\d+)$', range)
@@ -18,24 +20,27 @@ def load_corpus(range):
         from nltk.corpus import brown as corpus
         return [corpus.words(fileid) for fileid in corpus.fileids()[start:end]]
 
+
 def load_corpus_each_sentence(range):
     m = re.match(r'(\d+):(\d+)$', range)
     if m:
         start = int(m.group(1))
         end = int(m.group(2))
-        
+
 #        from nltk.corpus import brown as corpus
         from nltk.corpus import movie_reviews as corpus
         print ([corpus.sents(fileid) for fileid in corpus.fileids()[start:end]])
         return [corpus.sents(fileid) for fileid in corpus.fileids()[start:end]]
-        #return [corpus.sents("trial.txt")]
-        #sentence_tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
-        #sentence = sentence_tokenizer.tokenize(text)
-        
-        #from summarization import load_sample_text 
-        #text=load_sample_text(lines=2)
-        #return [nltk.word_tokenize(t) for t in nltk.sent_tokenize(text)]'''
-        #return [each_sent for each_sent in sentence]
+
+        # return [corpus.sents("trial.txt")]
+        # sentence_tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
+        # sentence = sentence_tokenizer.tokenize(text)
+
+        # from summarization import load_sample_text
+        # text=load_sample_text(lines=2)
+        # return [nltk.word_tokenize(t) for t in nltk.sent_tokenize(text)]'''
+        # return [each_sent for each_sent in sentence]
+
 
 def load_file(filename):
     corpus = []
@@ -47,30 +52,38 @@ def load_file(filename):
     f.close()
     return corpus
 
-#stopwords_list = nltk.corpus.stopwords.words('english')
+
+# stopwords_list = nltk.corpus.stopwords.words('english')
 stopwords_list = "a,s,able,about,above,according,accordingly,across,actually,after,afterwards,again,against,ain,t,all,allow,allows,almost,alone,along,already,also,although,always,am,among,amongst,an,and,another,any,anybody,anyhow,anyone,anything,anyway,anyways,anywhere,apart,appear,appreciate,appropriate,are,aren,t,around,as,aside,ask,asking,associated,at,available,away,awfully,be,became,because,become,becomes,becoming,been,before,beforehand,behind,being,believe,below,beside,besides,best,better,between,beyond,both,brief,but,by,c,mon,c,s,came,can,can,t,cannot,cant,cause,causes,certain,certainly,changes,clearly,co,com,come,comes,concerning,consequently,consider,considering,contain,containing,contains,corresponding,could,couldn,t,course,currently,definitely,described,despite,did,didn,t,different,do,does,doesn,t,doing,don,t,done,down,downwards,during,each,edu,eg,eight,either,else,elsewhere,enough,entirely,especially,et,etc,even,ever,every,everybody,everyone,everything,everywhere,ex,exactly,example,except,far,few,fifth,first,five,followed,following,follows,for,former,formerly,forth,four,from,further,furthermore,get,gets,getting,given,gives,go,goes,going,gone,got,gotten,greetings,had,hadn,t,happens,hardly,has,hasn,t,have,haven,t,having,he,he,s,hello,help,hence,her,here,here,s,hereafter,hereby,herein,hereupon,hers,herself,hi,him,himself,his,hither,hopefully,how,howbeit,however,i,d,i,ll,i,m,i,ve,ie,if,ignored,immediate,in,inasmuch,inc,indeed,indicate,indicated,indicates,inner,insofar,instead,into,inward,is,isn,t,it,it,d,it,ll,it,s,its,itself,just,keep,keeps,kept,know,knows,known,last,lately,later,latter,latterly,least,less,lest,let,let,s,like,liked,likely,little,look,looking,looks,ltd,mainly,many,may,maybe,me,mean,meanwhile,merely,might,more,moreover,most,mostly,much,must,my,myself,name,namely,nd,near,nearly,necessary,need,needs,neither,never,nevertheless,new,next,nine,no,nobody,non,none,noone,nor,normally,not,nothing,novel,now,nowhere,obviously,of,off,often,oh,ok,okay,old,on,once,one,ones,only,onto,or,other,others,otherwise,ought,our,ours,ourselves,out,outside,over,overall,own,particular,particularly,per,perhaps,placed,please,plus,possible,presumably,probably,provides,que,quite,qv,rather,rd,re,really,reasonably,regarding,regardless,regards,relatively,respectively,right,said,same,saw,say,saying,says,second,secondly,see,seeing,seem,seemed,seeming,seems,seen,self,selves,sensible,sent,serious,seriously,seven,several,shall,she,should,shouldn,t,since,six,so,some,somebody,somehow,someone,something,sometime,sometimes,somewhat,somewhere,soon,sorry,specified,specify,specifying,still,sub,such,sup,sure,t,s,take,taken,tell,tends,th,than,thank,thanks,thanx,that,that,s,thats,the,their,theirs,them,themselves,then,thence,there,there,s,thereafter,thereby,therefore,therein,theres,thereupon,these,they,they,d,they,ll,they,re,they,ve,think,third,this,thorough,thoroughly,those,though,three,through,throughout,thru,thus,to,together,too,took,toward,towards,tried,tries,truly,try,trying,twice,two,un,under,unfortunately,unless,unlikely,until,unto,up,upon,us,use,used,useful,uses,using,usually,value,various,very,via,viz,vs,want,wants,was,wasn,t,way,we,we,d,we,ll,we,re,we,ve,welcome,well,went,were,weren,t,what,what,s,whatever,when,whence,whenever,where,where,s,whereafter,whereas,whereby,wherein,whereupon,wherever,whether,which,while,whither,who,who,s,whoever,whole,whom,whose,why,will,willing,wish,with,within,without,won,t,wonder,would,would,wouldn,t,yes,yet,you,you,d,you,ll,you,re,you,ve,your,yours,yourself,yourselves,zero".split(',')
 recover_list = {"wa":"was", "ha":"has"}
 wl = nltk.WordNetLemmatizer()
 
+
 def is_stopword(w):
     return w in stopwords_list
+
+
 def lemmatize(w0):
     w = wl.lemmatize(w0.lower())
-    #if w=='de': print w0, w
-    if w in recover_list: return recover_list[w]
+    # if w=='de': print w0, w
+    if w in recover_list:
+        return recover_list[w]
     return w
+
 
 class Vocabulary:
     def __init__(self, excluds_stopwords=False):
-        self.vocas = [] # id to word
-        self.vocas_id = dict() # word to id
-        self.docfreq = [] # id to document frequency
+        self.vocas = []  # id to word
+        self.vocas_id = dict()  # word to id
+        self.docfreq = []  # id to document frequency
         self.excluds_stopwords = excluds_stopwords
 
     def term_to_id(self, term0):
         term = lemmatize(term0)
-        if not re.match(r'[a-z]+$', term): return None
-        if self.excluds_stopwords and is_stopword(term): return None
+        if not re.match(r'[a-z]+$', term):
+            return None
+        if self.excluds_stopwords and is_stopword(term):
+            return None
         if term not in self.vocas_id:
             voca_id = len(self.vocas)
 #            print str(voca_id) + ": " + term
@@ -82,37 +95,39 @@ class Vocabulary:
         return voca_id
 
     def doc_to_ids(self, doc):
-        #print ' '.join(doc)
-        list = []
+        # print ' '.join(doc)
+        term_list = []
         words = dict()
         for term in doc:
-            id = self.term_to_id(term)
-            if id != None:
-                list.append(id)
-                #if not words.has_key(id):
-                if id not in words:
-                    words[id] = 1
-                    self.docfreq[id] += 1
-        if "close" in dir(doc): doc.close()
-        return list
+            word_id = self.term_to_id(term)
+            if word_id is not None:
+                term_list.append(word_id)
+                # if not words.has_key(id):
+                if word_id not in words:
+                    words[word_id] = 1
+                    self.docfreq[word_id] += 1
+        if "close" in dir(doc):
+            doc.close()
+        return term_list
 
     def doc_to_ids_each_sentence(self, doc):
-        #print ' '.join(doc)
+        # print ' '.join(doc)
         sent_list = []
         words = dict()
-        
+
         for sent in doc:
-            list = []
+            term_list = []
             for term in sent:
-                id = self.term_to_id(term)
-                if id != None:
-                    list.append(id)
-                    #if not words.has_key(id):
-                    if id not in words:
-                        words[id] = 1
-                        self.docfreq[id] += 1
-            sent_list.append(list)
-        if "close" in dir(doc): doc.close()
+                word_id = self.term_to_id(term)
+                if word_id is not None:
+                    term_list.append(word_id)
+                    # if not words.has_key(id):
+                    if word_id not in words:
+                        words[word_id] = 1
+                        self.docfreq[word_id] += 1
+            sent_list.append(term_list)
+        if "close" in dir(doc):
+            doc.close()
         return sent_list
 
     def cut_low_freq(self, corpus, threshold=1):
@@ -134,7 +149,8 @@ class Vocabulary:
         def conv(doc):
             new_doc = []
             for id in doc:
-                if id in conv_map: new_doc.append(conv_map[id])
+                if id in conv_map:
+                    new_doc.append(conv_map[id])
             return new_doc
         return [conv(doc) for doc in corpus]
 
